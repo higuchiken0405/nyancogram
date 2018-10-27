4ef89dbb482711a75d8796bd68e0b37325a00107
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-10-27 17:08:52 +0900

    ユーザー一覧のServletとJSPを追加

---
+++ WebContent/WEB-INF/views/users/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="../layout/app.jsp">
+    <c:param name="content">
+        <h2>ユーザー一覧</h2>
+        <c:forEach var="user" items="${users}" >
+            <li>
+                <span><c:out value="${user.id}" /></span>
+                <span><c:out value="${user.name}" /></span>
+                <span><c:out value="${user.email}" /></span>
+                <span><c:out value="${user.password}" /></span>
+            </li>
+        </c:forEach>
+    </c:param>
+</c:import>
---
+++ src/controllers/user/UserIndexServlet.java
@@ -0,0 +1,44 @@
+package controllers.user;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.User;
+import utils.DBUtil;
+
+@WebServlet("/users/index")
+public class UserIndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public UserIndexServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	    //エンティティマネージャの生成
+	    EntityManager em = DBUtil.createEntityManger();
+	    //「全ユーザー情報を取得する」クエリを実行した結果を取得
+	    List<User> users = em.createNamedQuery("getAllUsers", User.class).getResultList();
+	    //エンティティマネージャの終了
+	    em.close();
+	    //全ユーザー情報をリクエストオブジェクトに格納
+	    request.setAttribute("users", users);
+	    //index.jspへ移動
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/index.jsp");
+	    rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
