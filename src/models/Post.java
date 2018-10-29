f864692e0f0bb2fea484b5fa9e49375bb70676f2
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-10-29 23:08:16 +0900

    投稿Postのクラスを追加

---
+++ src/models/Post.java
@@ -0,0 +1,83 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "",
+            query = ""
+            )
+})
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="title", nullable=false)
+    private String title;
+
+    @Column(name="content", nullable=false)
+    private String contetn;
+
+    @Column(name="image")
+    private String image;
+
+    @Column(name="user_id")
+    private Integer user_id;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContetn() {
+        return contetn;
+    }
+
+    public void setContetn(String contetn) {
+        this.contetn = contetn;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+
+
+}
