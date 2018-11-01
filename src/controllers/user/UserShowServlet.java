a00feecaef78bcd71b54abfc99a6918a243837d3
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-11-01 13:34:04 +0900

    ユーザー詳細画面への遷移の修正 ログインユーザーのiD やnullの時の対処

--- src/controllers/user/UserShowServlet.java
+++ src/controllers/user/UserShowServlet.java
@@ -25,14 +25,35 @@ public class UserShowServlet extends HttpServlet {
 
 	    //EntityManagerを生成
 	    EntityManager em = DBUtil.createEntityManger();
-	    //パラメータから取得したidの値で検索した結果を取得
-	    User user = em.find(User.class, Integer.parseInt(request.getParameter("id")));
+	    //パラメータからユーザーIDを取得
+	    Integer user_id = Integer.parseInt(request.getParameter("id"));
+	    //セッションオブジェクトからログインユーザーのIDを取得
+	    User login_user = (User) request.getSession().getAttribute("login_user");
+	    Integer login_user_id = login_user.getId();
+	    if(user_id == login_user_id) {
+	        //パラーメータのIDとログインユーザーのIDが一致する時
+	        //メイン画面へリダイレクト
+            response.sendRedirect(request.getContextPath() + "/");
+            return;
+	    }
+	    //パラメータから取得したIDで検索した結果を格納
+	    User user = em.find(User.class, user_id);
 	    //エンティティマネージャを終了
 	    em.close();
-	    //検索結果のuserをリクエストオブジェクトに格納
-	    request.setAttribute("user", user);
-	    //show.jspに移動
-	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/show.jsp");
-	    rd.forward(request, response);
+
+	    if(user == null) {
+	        //検索結果がnullの時
+            //ユーザー一覧へリダイレクト
+            response.sendRedirect(request.getContextPath() + "/users/index");
+            return;
+
+	    } else if (!user.equals(request.getSession().getAttribute("login_user"))) {
+	        //取得したuserがログインユーザーと一致しない場合
+	        //検索結果のuserをリクエストオブジェクトに格納
+	        request.setAttribute("user", user);
+	        //show.jspに移動
+	        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/show.jsp");
+	        rd.forward(request, response);
+	    }
 	}
 }
