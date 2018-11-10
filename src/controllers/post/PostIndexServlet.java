26a41eded206407f9a144ea457e81b643c6225d0
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-11-11 03:22:44 +0900

    ポスト一覧のServletを追加

---
+++ src/controllers/post/PostIndexServlet.java
@@ -0,0 +1,63 @@
+package controllers.post;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Comment;
+import models.Post;
+import models.StringToArray;
+import utils.DBUtil;
+
+@WebServlet("/posts/index")
+public class PostIndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public PostIndexServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+	    //エンティティマネージャの生成
+	    EntityManager em = DBUtil.createEntityManger();
+	    //「全ての投稿を取得する」クエリを実行した結果を取得
+	    List<Post> posts = em.createNamedQuery("getAllPosts", Post.class).getResultList();
+	    //「全てのコメントを取得する」クエリを実行した結果を取得
+	    List<Comment> comments = em.createNamedQuery("getAllComments", Comment.class).getResultList();
+	    //エンティティマネージャを終了
+	    em.close();
+
+        //文字列を改行で分けて配列に変換し、セットする
+        if(posts != null) {
+            for(Post post:posts) {
+                post.setContent_array(StringToArray.contentToArray(post));
+            }
+        }
+        if(comments != null) {
+            for(Comment comment:comments) {
+                comment.setBody_array(StringToArray.bodyToArray(comment));
+            }
+        }
+
+	    //リクエストオブジェクトに実行結果を格納
+	    request.setAttribute("posts", posts);
+	    request.setAttribute("comments", comments);
+	    //投稿一覧画面へ遷移
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/posts/index.jsp");
+	    rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
