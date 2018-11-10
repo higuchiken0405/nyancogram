d12e106271b075a968f8b6002d1096ec28757da3
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-11-11 03:31:40 +0900

    ログアウトのServletを追加

---
+++ src/controllers/logout/LogoutServlet.java
@@ -0,0 +1,31 @@
+package controllers.logout;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LogoutServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	    //セッションからログインユーザーを削除
+	    request.getSession().removeAttribute("login_user");
+	    //セッションオブジェクトごと除去
+	    request.getSession().invalidate();
+	    //トップページへリダイレクト
+	    response.sendRedirect(request.getContextPath() + "/");
+
+	}
+
+
+}
